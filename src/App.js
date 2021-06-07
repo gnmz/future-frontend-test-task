3ec2450d076c697cfd183b034eb8832af18ecc4d
gnmz <i.popov.developer@gmail.com>
2021-06-07 17:20:00 +0300

    create function getSmallData

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 export class App extends Component {
   state = {
-    data: []
+    data: [],
+  };
+
+  componentDidMount() {
+
   }
 
-
+  getSmallData = () => {
+    let url = `http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.setState({data: data}));
+  };
 
   render() {
-    return (
-      <div className="app">
-        
-      </div>
-    )
+  
+    return <div className="app"></div>;
   }
 }
 
-export default App
+export default App;
