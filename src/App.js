e5ba83443aa9d50439fe3c1c2a669d99d14aeed9
gnmz <i.popov.developer@gmail.com>
2021-06-09 02:40:24 +0300

    finished loader component

--- src/App.js
+++ src/App.js
@@ -2,57 +2,81 @@ import React, { Component } from "react";
 import ReactPaginate from "react-paginate";
 import "./App.css";
 import ActionModeSelector from "./components/ActionModeSelector";
+import Loader from "./components/Loader";
 import Table from "./components/Table";
 
 export class App extends Component {
   state = {
     data: [],
+    isClicked: false,
+    isLoading: true,
     pageSize: 50,
-    currentPage: 0
+    currentPage: 0,
   };
 
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
 
   fetchSmallData = () => {
+    this.checkAction();
     let url = `http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => this.setState({ data: data }));
+      .then((data) => this.setState({ isLoading: false, data: data }));
   };
 
   fetchBigData = () => {
+    this.checkAction();
     let url = `http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => this.setState({ data: data }));
+      .then((data) => this.setState({ isLoading: false, data: data }));
+  };
+
+  checkAction = () => {
+    if (this.state.data) {
+      this.setState({ isLoading: true });
+    }
+    if (!this.state.isClicked) {
+      this.setState({ isClicked: true });
+    }
   };
 
   render() {
-    const { data, pageSize } = this.state;
+    const { data, pageSize, isLoading } = this.state;
     return (
       <div className="app">
-        <ActionModeSelector fetchSmallData={this.fetchSmallData} fetchBigData={this.fetchBigData} />
-        <Table data={data} />
-        <ReactPaginate
-          previousLabel={"Prev"}
-          nextLabel={"Next"}
-          breakLabel={"..."}
-          breakClassName={"break-me"}
-          pageCount={data.length / pageSize}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={this.handlePageClick}
-          containerClassName={"pagination"}
-          activeClassName={"active"}
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          nextClassName="page-item"
-          previousLinkClassName="page-link"
-          nextLinkClassName="page-link"
+        <ActionModeSelector
+          fetchSmallData={this.fetchSmallData}
+          fetchBigData={this.fetchBigData}
         />
+        {isLoading ? (
+          this.state.isClicked ? (
+            <Loader />
+          ) : null
+        ) : (
+          <>
+            <Table data={data} />
+
+            <ReactPaginate
+              previousLabel={"Prev"}
+              nextLabel={"Next"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={data.length / pageSize}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.handlePageClick}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+              previousClassName="page-item"
+              nextClassName="page-item"
+              previousLinkClassName="page-link"
+              nextLinkClassName="page-link"
+            />
+          </>
+        )}
       </div>
     );
   }
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export class Loader extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="loader">
+                <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             </div>
+            
         )
     }
 }
