0caffba4b472198afca055f1b22b4cf084291f9f
gnmz <i.popov.developer@gmail.com>
2021-06-09 04:50:49 +0300

    create TableSearch component

---
+++ src/components/TableSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TableSearch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TableSearch
